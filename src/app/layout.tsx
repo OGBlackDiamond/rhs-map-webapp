9ae70f2237da9ba9eb1c728a29bf9adb5ec649d8
OGBlackDiamond <caden.feller@gmail.com>
2023-11-14 22:34:22 -0800

    progress kinda

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,34 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import { AppBar, Container } from '@mui/material'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Reno High Schedule Mapper',
   description: 'A tool to help you map out your schedule!',
 }
+
+function Header() {
+  return (
+    <AppBar position='static' style={
+      {
+        color: "black",
+        textAlign: "left",
+        border: "auto",
+        height: "100px"
+      }
+    }>
+      <Container  maxWidth='xl'>
+        <div style={{padding: "100px"}}>
+          Show me something
+        </div>
+      </Container>
+    </AppBar>
+  );
+}
+
 
 export default function RootLayout({
   children,
@@ -16,7 +38,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
